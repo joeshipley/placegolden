0db6780ffc9e40553f76986b0e6fa9e7b494ef7c
cmmerritt <christiane.merritt@gmail.com>
2021-09-12 00:56:18 -0700

    move header to a component; add css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>PlaceGolden</h1>
-      </header>
+      <Header />
       <body>
         <p>
           Placeholder images of the Golden Girls. Just a few below. Coming soon: choose image dimensions and grayscale.
