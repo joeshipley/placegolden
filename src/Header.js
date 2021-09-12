fc30411a1f782e91178f37ec8eeb0527fe6c2ef7
cmmerritt <christiane.merritt@gmail.com>
2021-09-12 15:55:09 -0700

    add styling to header

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,32 @@
 import './Header.css';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  header: {
+    backgroundColor: "rgb(161, 232, 204)",
+    color: "rgb(73, 81, 89)",
+  },
+  logo: {
+    fontFamily: "monotypeCorsiva",
+  },
+}));
 
 function Header() {
+  const { header, logo } = useStyles();
+
   const displayDesktop = () => {
     return <Toolbar>{placegoldenLogo}</Toolbar>;
   };
 
   const placegoldenLogo = (
-    <Typography variant="h4" component="h1">
+    <Typography variant="h4" component="h1" className={logo}>
       PlaceGolden
     </Typography>
   );
 
   return (
-    <div className="header">
-      <AppBar>{displayDesktop()}</AppBar>
+    <div>
+      <AppBar className={header}>{displayDesktop()}</AppBar>
     </div>
   );
 }
