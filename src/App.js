e30e5e6ed087c1b65c78181da59b3f2243ad8446
cmmerritt <christiane.merritt@gmail.com>
2021-09-21 00:14:11 -0700

    refactor App file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import Header from './Header';
 import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import {
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <>
       <Header />
-      <HomePage />
-    </div>
+
+      <Switch>
+
+        <Route path="/" exact={true} className="App"
+          render={routerProps => (
+            <HomePage {...routerProps} />
+          )}
+        />
+
+        <Route path="/about" exact={true}
+          render={routerProps => (
+            <AboutPage {...routerProps} />
+          )}
+        />
+
+        <Redirect to="/" />
+
+      </Switch>
+    </>
   );
 }
 
