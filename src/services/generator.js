cc13238efeb5392e3e5f1db272f2d7256d74049a
cmmerritt <christiane.merritt@gmail.com>
2021-09-26 01:33:48 -0700

    work on image function

--- src/services/generator.js
+++ src/services/generator.js
@@ -1,23 +1,22 @@
 import * as fs from 'fs';
 import express from 'express';
 import * as path from 'path';
-// import * as serverless from 'serverless-http';
+import * as serverless from 'serverless-http';
 
-const app = express();
+export const app = express();
 const router = express.Router();
 
 const currentDir = process.env.LAMBDA_TASK_ROOT || __dirname;
 
-app.get('/', (req, res) => {
-  const params = req.path.split(/[/x]+/);
+app.get('/*', (req, res) => {
+  const params = req.path.split(/[\/x]+/);
   fs.readFile(path.join(currentDir, 'images.json'), 'utf-8', (error, data) => {
     if(error || isNaN(params[0])) return res.redirect('/about');
 
       const images = JSON.parse(data);
 
-      let width, height, ratio = params[2];
-
-      width = params[0];
+      let width = params[0];
+      let height = width;
 
       if(!isNaN(params[1])) {
         height = params[1];
@@ -25,7 +24,7 @@ app.get('/', (req, res) => {
         height = width;
       }
 
-      ratio = width/height;
+      let ratio = width/height;
 
       let getImageAttempts = 0;
       const getImage = () => {
@@ -45,11 +44,20 @@ app.get('/', (req, res) => {
         }
         const image = getImage();
 
+        console.log(image);
+
         res.writeHead(200, {'Content-Type': 'image/svg+xml', 'Cache-Control': 'public, max-age=86400'});
     
         return res.end('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + width + '" height="' + height + '" viewBox="0 0 ' + image.width + ' ' + image.height + '" preserveAspectRatio="xMidYMid slice"><image xlink:href="' + image.data + '" width="' + image.width + '" height="' + image.height + '" /></svg>');
   });
 });
 
-app.use('/.netlify/functions/generator', router);
-
+app.use(router);
+// module.exports.handler serverless
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  // you can do other things here
+  const result = await handler(event, context);
+  // and here
+  return result;
+};
