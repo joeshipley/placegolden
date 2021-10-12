b724ad8fb4ae3c2c4f31c8940d92320d39f362c3
cmmerritt <christiane.merritt@gmail.com>
2021-10-11 23:14:28 -0700

    get json object on back end

--- src/components/images/ImageDisplay.jsx
+++ src/components/images/ImageDisplay.jsx
@@ -2,13 +2,13 @@ import { useParams } from 'react-router-dom';
 import { fetchImageByParams } from '../../services/imageService';
 
 const ImageDisplay = () => {
-  const { height, width } = useParams();
-  const image = fetchImageByParams(height, width);
+  const { width, height } = useParams();
+  const image = fetchImageByParams(width, height);
   console.log(height);
   console.log(image);
   return (
     <>
-      Now showing image with dimensions {height} {width}
+      Now showing image with dimensions {width} {height}
       <object data={image.data} type="image/svg+xml">
       </object>
     </>
