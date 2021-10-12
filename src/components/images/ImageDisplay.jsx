a3a62a0b6557e24c2342b5d305d8582f407e4837
Joe Shipley <joes@rightline.com>
2021-10-12 00:56:06 -0700

    Displays but infinite loop now, you can fix that

--- src/components/images/ImageDisplay.jsx
+++ src/components/images/ImageDisplay.jsx
@@ -1,18 +1,23 @@
+import React, { useState } from "react";
 import { useParams } from 'react-router-dom';
 import { fetchImageByParams } from '../../services/imageService';
 
-const ImageDisplay = () => {
+const ImageDisplay = (props) => {
   const { width, height } = useParams();
-  const image = fetchImageByParams(width, height);
-  console.log(height);
-  console.log(image);
+  const [state, setState] = useState({data: ""});
+  fetchImageByParams(width, height, function(retCallback){
+      setState({data: retCallback.data})
+  });
+  //console.log(height);
+  console.log("IMAGE LOADED?")
   return (
-    <>
-      Now showing image with dimensions {width} {height}
-      <object data={image.data} type="image/svg+xml">
-      </object>
-    </>
+      <>
+        Now showing image with dimensions {width} {height}
+        <object data={state.data} type="image/svg+xml">
+        </object>
+      </>
   )
+
 }
 
 export default ImageDisplay;
