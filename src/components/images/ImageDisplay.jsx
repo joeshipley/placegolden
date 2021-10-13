f9ac01f67a413075897ebca4e8a2c61cb6cc0299
Joe Shipley <joes@rightline.com>
2021-10-12 19:38:03 -0700

    Successful proxying to backend under localhost:3000/api/512/512 with setupProxy http-proxy-middlewear package

--- src/components/images/ImageDisplay.jsx
+++ src/components/images/ImageDisplay.jsx
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
 import { useParams } from 'react-router-dom';
-import { fetchImageByParams } from '../../services/imageService';
 
 const ImageDisplay = (props) => {
   const { width, height } = useParams();
-  const [state, setState] = useState({data: ""});
-  fetchImageByParams(width, height, function(retCallback){
-      setState({data: retCallback.data})
-  });
-  //console.log(height);
-  console.log("IMAGE LOADED?")
   return (
       <>
         Now showing image with dimensions {width} {height}
-        <object data={state.data} type="image/svg+xml">
-        </object>
+        <img src='/{width}/{height}' alt="whatever">
+        </img>
       </>
   )
 
