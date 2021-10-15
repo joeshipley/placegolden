c95393304ed06ee1397a051c479d57e693d92e25
Joe Shipley <joes@rightline.com>
2021-10-14 19:06:25 -0700

    Lambda works, but images are wrong size, none of the sizing logic worked so I just cut it out

--- src/api/server.js
+++ src/api/server.js
@@ -1,32 +1,27 @@
-import express from 'express';
-import {base64Sync} from 'base64-img';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import * as fs from 'fs';
-import images from '../../images/images.js';
-import serverless from 'serverless-http';
+const serverless = require('serverless-http');
+const express = require('express');
+const images = require('../../images/images.json');
 
 const app = express();
-
 const router = express.Router();
 
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-router.get('/api/:width/:height', (req, res, next) => {
+router.get('/:width/:height', (req, res, next) => {
   const { width, height } = req.params;
   let imageData1 = '';
-
   let ratio = width/height;
-
   let getImageAttempts = 0;
+  const yourImage = images[Math.floor(Math.random() * images.length)];
+  /* You are doing a random search here to find matching images... And this code doesn't work, getImage is not defined after it runs.
   const getImage = () => {
     const yourImage = images[Math.floor(Math.random() * images.length)];
+    //console.log(yourImage);
     const yourImageRatio = yourImage.width/yourImage.height;
     if((ratio < 0.8 && yourImageRatio > 1) || (ratio > 1.2 && yourImageRatio < 1)) {
       getImageAttempts++;
@@ -37,26 +32,17 @@ router.get('/api/:width/:height', (req, res, next) => {
       }
     }
   }
-
-  const image = getImage();
-  imageData1 = image.data;
-
-  let base64Data = imageData1.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
-
-  const img = Buffer.from(base64Data, 'base64');
-
-  res.writeHead(200, {
-  'Content-Type': 'image/jpeg',
-  'Content-Length': img.length
-  });
-
-  res.end(img);
+  */
+  /*Turns out netlify doesn't support binary data, so we have to it the same was as place keanu.*/
+  res.writeHead(200, {'Content-Type': 'image/svg+xml', 'Cache-Control': 'public, max-age=86400'});
+  return res.end(
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'+ width + '" height="' + height +
+      '" viewBox="0 0 ' + yourImage.width + ' ' + yourImage.height +
+      '" preserveAspectRatio="xMidYMid slice"> ' +
+      '<image xlink:href="' + yourImage.data + '" width="' + yourImage.width + '" height="' + yourImage.height + '" /></svg>');
 });
 
-const PORT = process.env.PORT || 9000;
+app.use('/.netlify/functions/server', router);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.use('/.netlify/functions/generator', router);
-
+module.exports = app;
 module.exports.handler = serverless(app);
